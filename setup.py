de40f95b8a1b4c232bf5859ebacbeff24f052e40
Angie Yoon <baicalein@gmail.com>
2024-10-30 17:28:18 -0400

    send try after pip install

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="Demo",
     version="0.1",
     description="A simple example package", 
-    url=’https://github.com/baicalein/M09-demo-2024.git’,
+    url="https://github.com/baicalein/M09-demo-2024.git",
 author="Heejeong Angie Yoon", 
 author_email="your.email@example.com", 
 license="MIT", 
-packages=[‘demo’]
+packages=["demo"]
 )
