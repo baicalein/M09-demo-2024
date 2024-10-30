13d9f2dbef9e0eb65e92cf30783724c529eb8c70
Angie Yoon <baicalein@gmail.com>
2024-10-30 17:00:10 -0400

    First try

---
+++ demo/mymodule.py
@@ -0,0 +1,2 @@
+def greet():
+    print("Hello, this a demo of package")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Demo",
+    version="0.1",
+    description="A simple example package", 
+    url=’https://github.com/baicalein/M09-demo-2024.git’,
+author="Heejeong Angie Yoon", 
+author_email="your.email@example.com", 
+license="MIT", 
+packages=[‘demo’]
+)
